f9e98a5dea28c4276f85e24c86052d16c935f883
chojuninengu <chojuninengu@gmail.com>
2025-06-20 11:00:42 +0100

    Update default JWT secret to a secure randomly generated key and ensure proper byte conversion for encoding and decoding keys.

--- src/config/jwt.rs
+++ src/config/jwt.rs
@@ -2,7 +2,8 @@ use std::env;
 use jsonwebtoken::{EncodingKey, DecodingKey};
 
 // Default key to use if environment variables are not set
-const DEFAULT_JWT_SECRET: &[u8] = b"default_secret_key_please_change_in_production";
+// Using a secure randomly generated key
+const DEFAULT_JWT_SECRET: &str = "7aecf547f7283d58405abe08d8886b1e1b69737f8eb3482facdadffa5be34d3e00d5e0b04a13daf7ed82346b277e8004b4c69be8ccf2291ac0e24f06e2a07871";
 
 /// Get the JWT encoding key from the environment or use a default for development
 pub fn get_encoding_key() -> EncodingKey {
@@ -11,7 +12,7 @@ pub fn get_encoding_key() -> EncodingKey {
         Err(_) => {
             // In production, you might want to panic here instead of using a default
             println!("Warning: Using default JWT secret. This is not secure for production!");
-            EncodingKey::from_secret(DEFAULT_JWT_SECRET)
+            EncodingKey::from_secret(DEFAULT_JWT_SECRET.as_bytes())
         }
     }
 }
@@ -23,7 +24,7 @@ pub fn get_decoding_key() -> DecodingKey {
         Err(_) => {
             // In production, you might want to panic here instead of using a default
             println!("Warning: Using default JWT secret. This is not secure for production!");
-            DecodingKey::from_secret(DEFAULT_JWT_SECRET)
+            DecodingKey::from_secret(DEFAULT_JWT_SECRET.as_bytes())
         }
     }
 } 
