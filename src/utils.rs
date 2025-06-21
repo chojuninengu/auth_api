5189b07951f46bf879f32b31f32737a8dcc1fc34
chojuninengu <chojuninengu@gmail.com>
2025-06-21 09:22:40 +0100

    Refactor environment variable loading in Config struct for improved clarity and error handling

--- src/utils.rs
+++ src/utils.rs
@@ -1,33 +1,23 @@
 use dotenvy::dotenv;
-
-pub struct Config {
+pub struct  Config{
     pub jwt_salt: String,
     pub jwt_secret: String,
-    pub jwt_expiration: String,
+    pub jwt_expiration_secs: u32,
 }
 
 pub fn load_env() -> Config {
     dotenv().ok();
 
-    let JWT_SALT = std::env::var("JWT_SALT")
-        .unwrap_or_else(|_| {
-            println!("JWT_SALT must be set in .env file");
-            std::process::exit(1);
-        });
-    let JWT_SECRET = std::env::var("JWT_SECRET")
-        .unwrap_or_else(|_| {
-            println!("JWT_SECRET must be set in .env file");
-            std::process::exit(1);
-        });
-    let JWT_EXPIRATION = std::env::var("JWT_EXPIRATION")
-        .unwrap_or_else(|_| {
-            println!("JWT_EXPIRATION must be set in .env file");
-            std::process::exit(1);
-        });
+    let jwt_salt = std::env::var("JWT_SALT").expect("JWT_SALT environment variable is not set");
+    let jwt_secret = std::env::var("JWT_SECRET").expect("JWT_SECRET environment variable is not set");
+    let jwt_expiration_secs = std::env::var("JWT_EXPIRATION_SECS")
+    .expect("JWT_EXPIRATION_SECS environment variable is not set")
+    .parse::<u32>()
+    .expect("JWT_EXPIRATION_SECS must be a valid unsigned integer");
 
-    return Config {
-        jwt_salt: JWT_SALT,
-        jwt_secret: JWT_SECRET,
-        jwt_expiration: JWT_EXPIRATION,
-    };
+return Config{
+    jwt_salt,
+    jwt_secret,
+    jwt_expiration_secs,
+};
 }
